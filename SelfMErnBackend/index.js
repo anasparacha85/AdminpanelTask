7aeaec9981bca31740da809a6c0116e11110bb42
SyedTalha71x <syedtalha71x@gmail.com>
2025-01-28 00:47:34 +0500

    update

--- SelfMErnBackend/index.js
+++ SelfMErnBackend/index.js
@@ -1,29 +1,28 @@
-const express=require('express');
-const server=express();
-const router=require('./Router/Auth-router')
-const servocerouter=require('./Router/Service router')
-const adminrouter=require('./Router/Admin-Router')
-const shoprouter=require('./Router/Shop-router')
-const cors=require('cors')
-const errormiddleware=require('./Middleware/error-Middleware')
-const dbconnect=require('./utils/db')
-const bodyparser=require('body-parser')
+const express = require('express');
+const server = express();
+const router = require('./Router/Auth-router');
+const servocerouter = require('./Router/Service router');
+const adminrouter = require('./Router/Admin-Router');
+const shoprouter = require('./Router/Shop-router');
+const cors = require('cors');
+const errormiddleware = require('./Middleware/error-Middleware');
+const dbconnect = require('./utils/db');
+const bodyparser = require('body-parser');
 const dotenv = require('dotenv');
-server.use(cors())
-server.use(bodyparser.json())
+
+server.use(cors());
+server.use(bodyparser.json());
 
 dotenv.config();
 
+server.use('/api/auth', router);
+server.use('/api/Services', servocerouter);
+server.use('/api/admin', adminrouter);
+server.use('/api/Shop', shoprouter);
+server.use(errormiddleware);
 
-server.use('/api/auth',router)
-server.use('/api/Services',servocerouter)
-server.use('/api/admin',adminrouter)
-server.use('/api/Shop',shoprouter)
-server.use(errormiddleware)
-dbconnect().then(()=>{
-    server.listen(process.env.PORT,()=>{
+dbconnect().then(() => {
+    server.listen(process.env.PORT, '0.0.0.0', () => {
         console.log('server started');
-        
-    })
-})
-
+    });
+});
